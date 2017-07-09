7d85e4dc087a78ace82b3c9212d1ff7e81d10141
Satoshi Morinaga <Satoahi@Satoshi-no-MacBook-Air.local>
2017-07-09 15:21:52 +0900

    test

---
+++ js/components/chat_test.vue
@@ -0,0 +1,50 @@
+te>
+    <div id="content">
+        <p>チャットぺージ</p>
+        <p>チャット画面を表示するテストページ</p>
+
+    <div id="attack_list" v-if="command_state === 'コメント'">
+        <ul>
+            <li v-for="a in attack_list">
+                {{ a }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="item_list" v-if="command_state === 'エリア情報'">
+        <ul>
+            <li v-for="i in item_list">
+                {{ i }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="magic_list" v-if="command_state === 'いいね数'">
+        <ul>
+            <li v-for="m in magic_list">
+                {{ m }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="nigeru" v-if="command_state === 'お気に入り数'">
+        <p></p>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'third',
+    mounted: function() {
+        this.$store.dispatch('load_test_api');
+    },
+    computed: {
+        list: function() {
+            return this.$store.state.api_data;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
