d919804d0331bb7b0c2ba58c01d912519c2e67b0
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-10 19:33:02 +0900

    test

--- js/components/First.vue
+++ js/components/First.vue
@@ -1,8 +1,44 @@
-//検索機能
 <template>
 <div id="content">
     <input v-model="command" placeholder="select command"　@keyup.enter="select_command">
-    <i class="fa fa-search" aria-hidden="true"></i>
+    <i class="fa fa-gratipay" aria-hidden="true"></i>
+
+    <div id="attack_list" v-if="command_state === ''">
+        <ul>
+            <li v-for="c in command_list">
+                {{ c }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="attack_list" v-if="command_state === 'たたかう'">
+        <ul>
+            <li v-for="a in attack_list">
+                {{ a }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="item_list" v-if="command_state === 'どうぐ'">
+        <ul>
+            <li v-for="i in item_list">
+                {{ i }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="magic_list" v-if="command_state === 'まほう'">
+        <ul>
+            <li v-for="m in magic_list">
+                {{ m }}
+            </li>
+        </ul>
+    </div>
+
+    <div id="nigeru" v-if="command_state === 'にげる'">
+        <p>にげられない</p>
+    </div>
+</div>
 </template>
 
 <script>
@@ -11,6 +47,24 @@ export default {
     data: function() {
         return {
             command: "",
+        }
+    },
+
+    computed: {
+        command_state: function() {
+            return this.$store.state.command;
+        },
+        command_list: function() {
+            return this.$store.state.command_list;
+        },
+        attack_list: function() {
+            return this.$store.state.attack_list;
+        },
+        item_list: function() {
+            return this.$store.state.item_list;
+        },
+        magic_list: function() {
+            return this.$store.state.magic_list;
         }
     },
 
