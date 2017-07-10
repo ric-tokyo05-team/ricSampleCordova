dba00b56513db7e7c11e3e87f4e06beac86b26f5
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-10 19:31:25 +0900

    test

--- js/components/Search_text.vue
+++ js/components/Search_text.vue
@@ -1,1 +1,32 @@
 //検索機能
+<template>
+<div id="content">
+    <input v-model="command" placeholder="select command"　@keyup.enter="select_command">
+    <i class="fa fa-search" aria-hidden="true"></i>
+</template>
+
+<script>
+export default {
+    name: 'First',
+    data: function() {
+        return {
+            command: "",
+        }
+    },
+
+    methods: {
+        select_command(event) {
+            this.$store.commit("select_command", this.command);
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#content {
+    width: 100%;
+    height: 2000px;
+    margin-top: 60px;
+}
+
+</style>
