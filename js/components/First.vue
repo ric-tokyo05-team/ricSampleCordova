68701f7c22310739b45ab752f24fcafca81f05d3
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-27 22:53:54 +0900

    test

--- js/components/First.vue
+++ js/components/First.vue
@@ -4,8 +4,7 @@
     <input v-model="command" placeholder="新宿 ディナー"　@keyup.enter="select_command">
     <input type="submit" value="&#xf002;">
     <input type="submit" class="icon-search">
-
-        <span class="icon-search"></span>
+    <i class="icon-search" >
 
     <div id="attack_list" v-if="command_state === ''">
         <ul>
