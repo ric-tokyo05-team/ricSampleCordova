96350050e49686593fc6b295e97c00df1fc50234
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-30 00:33:35 +0900

    test

--- js/components/First.vue
+++ js/components/First.vue
@@ -198,8 +198,8 @@ export default {
     width: 0px;
     height: 0px;
     border-style: solid;
-    border-width: 10px 10px 10px 10px;
-    border-color: transparent #D9F0FF transparent transparent;
+    border-width: 10px 10px 0 10px;
+    border-color: transparent transparent #D9F0FF transparent;
 }
 
 .someone {
