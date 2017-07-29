00cb52317b690b48a4113ae1e8259f4a83068db1
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-30 01:25:08 +0900

    test

--- js/components/First.vue
+++ js/components/First.vue
@@ -171,7 +171,7 @@ export default {
 .chat-hukidashi-friend {
     display: inline-block; /*コメントの文字数に合わせて可変*/
     padding: 15px 20px;
-    margin-right: 7t0px;
+    margin-right: 70px;
     /* border: 1px solid gray; ←削除 */
     border-radius: 10px;
     position: relative; /*追記*/
