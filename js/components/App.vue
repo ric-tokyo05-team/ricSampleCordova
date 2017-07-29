25a7b3b3625f146ddaa80297b74ec4d0d17b671c
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-29 23:38:49 +0900

    test

--- js/components/App.vue
+++ js/components/App.vue
@@ -8,14 +8,12 @@
 
 <script>
 import AppHeader from "./Header.vue"
-import Appchat from "./chat.vue"
 import AppFooter from "./Footer.vue"
 
 export default {
   name: "app",
   components: {
     "appheader": AppHeader,
-    "appchat" :Appchat,
     "appfooter": AppFooter,
   }
 }
