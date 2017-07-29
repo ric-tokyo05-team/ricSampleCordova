c2d8bf91c25ac997ffacaee923f7c23eb1f49c56
竹川美雪 <takekawamiyuki@take-MacBook-Pro.local>
2017-07-30 01:54:53 +0900

    test

--- js/components/First.vue
+++ js/components/First.vue
@@ -198,6 +198,7 @@ export default {
     display: inline-block; /*コメントの文字数に合わせて可変*/
     padding: 15px 20px;
     margin-left: 70px;
+    margin-bottom: 10px;
     /* border: 1px solid gray; ←削除 */
     border-radius: 10px;
     position: relative; /*追記*/
